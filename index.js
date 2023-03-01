3cb5f2779c3f3db02172f041801ef98f39c99fda
shivam singh <126107873+Shivamsin517@users.noreply.github.com>
2023-03-01 06:59:21 +0000

    docs:used chalk to get color

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-console.log('lets play a game how much you know me')
+ var chalk = require('chalk');
+console.log(chalk.green('lets play a game how much you know me'))
 var readlineSync=require('readline-sync')
+ var chalk = require('chalk');
 var score=0
 var userUttar=readlineSync.question('whats your name?')
-console.log('welcome '+userUttar)
+console.log(chalk.red('welcome '+userUttar))
 function play(question,answer){
   var userUttar=readlineSync.question(question)
-  if (userUttar===answer){score=score+1;console.log('correct ','score: '+score)}
-  else{console.log('wrong')}
-  console.log('total score: '+score)
+  if (userUttar===answer){score=score+1;console.log(chalk.blue('correct ','score: '+score))}
+  else{console.log(chalk.blue('wrong'))}
+  console.log(chalk.blue('total score: '+score))
 }
 //play('where do you live?','amethi')
 //play('what is my nickname?','chotu')
 //play('which politician i like most?','modi')
 //play('best singer?','arijit singh')
 //play('best quality i have?','honesty')
-var questionOne={question:'where do you live?',
+var questionOne={question:'where do i live?',
               answer:'amethi'}
 var questionTwo={question:'what is my nickname?',
               answer:'chotu'}
@@ -24,8 +26,10 @@ var questionFour={question:'best singer?',
               answer:'arijit singh'}
 var questionFive={question:'best quality i have?',
               answer:'honesty'}
-var questions=[questionOne,questionTwo,questionThree,questionFour,questionFive]
-for(var i=0;i<5;i++){
+var questionSix={question:'am i commited?',
+              answer:'no'}
+var questions=[questionOne,questionTwo,questionThree,questionFour,questionFive,questionSix]
+for(var i=0;i<questions.length;i++){
   var currentData=questions[i]
   console.log(play(currentData.question,currentData.answer))
   

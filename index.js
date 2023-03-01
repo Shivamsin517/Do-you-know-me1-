b695b9ec01f062d866748ddad2385c8fb5335b4c
shivam singh <126107873+Shivamsin517@users.noreply.github.com>
2023-03-01 06:43:34 +0000

    docs:applied objects,array and for loop

--- index.js
+++ index.js
@@ -7,10 +7,28 @@ function play(question,answer){
   var userUttar=readlineSync.question(question)
   if (userUttar===answer){score=score+1;console.log('correct ','score: '+score)}
   else{console.log('wrong')}
+  console.log('total score: '+score)
 }
-play('where do you live?','amethi')
-play('what is my nicknae?','chotu')
-play('which politician i like most?','modi')
-play('best singer?','arijit singh')
-play('best quality i have?','honesty')
+//play('where do you live?','amethi')
+//play('what is my nickname?','chotu')
+//play('which politician i like most?','modi')
+//play('best singer?','arijit singh')
+//play('best quality i have?','honesty')
+var questionOne={question:'where do you live?',
+              answer:'amethi'}
+var questionTwo={question:'what is my nickname?',
+              answer:'chotu'}
+var questionThree={question:'which politician i like most?',
+              answer:'modi'}
+var questionFour={question:'best singer?',
+              answer:'arijit singh'}
+var questionFive={question:'best quality i have?',
+              answer:'honesty'}
+var questions=[questionOne,questionTwo,questionThree,questionFour,questionFive]
+for(var i=0;i<5;i++){
+  var currentData=questions[i]
+  console.log(play(currentData.question,currentData.answer))
+  
+  
+}
 

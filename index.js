df57f60cc73cb72f58285754a683171c0b8665dd
shivam singh <126107873+Shivamsin517@users.noreply.github.com>
2023-03-01 04:48:20 +0000

    docs:added welcome message

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('lets play a game how much you know me')
+var readlineSync=require('readline-sync')
+var userUttar=readlineSync.question('whats your name?')
+console.log('welcome '+userUttar)
+

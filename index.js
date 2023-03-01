59e86edb0d963dd081e5e55c79a14a4dc7ddd4f0
shivam singh <126107873+Shivamsin517@users.noreply.github.com>
2023-03-01 05:24:37 +0000

    docs:added question and asking it by using function only

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 console.log('lets play a game how much you know me')
 var readlineSync=require('readline-sync')
+var score=0
 var userUttar=readlineSync.question('whats your name?')
 console.log('welcome '+userUttar)
+function play(question,answer){
+  var userUttar=readlineSync.question(question)
+  if (userUttar===answer){score=score+1;console.log('correct ','score: '+score)}
+  else{console.log('wrong')}
+}
+play('where do you live?','amethi')
+play('what is my nicknae?','chotu')
+play('which politician i like most?','modi')
+play('best singer?','arijit singh')
+play('best quality i have?','honesty')
 
